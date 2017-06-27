758152cd925ef4a479ee70c2a04dc15559372770
emvcoder <emvcoder@gmail.com>
2017-06-27 22:31:14 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let delay = 400; // 0.4 sec
+
+window.onload = () => {
+  let showPreloader = () => {
+    document.querySelector(".spinner").style.display = "block"
+    document.querySelector(".spinner").style.background = "rgba(255, 255, 255, .93)";
+    document.querySelector(".fidget_spinner").style = "";
+  }
+  let hidePreloader = () => {
+    document.querySelector(".spinner").style.background = "transparent";
+    document.querySelector(".fidget_spinner").style.background = "transparent";
+    setTimeout(() => { document.querySelector(".spinner").style.display = "none" }, 150)
+  };
+
+  let preloader = setTimeout(hidePreloader, delay);
+
+  document.querySelector("#show__preloader").onclick = showPreloader;
+};
