532d86d98248939fc03b4af753b1aea593eaf22e
emvcoder <emvcoder@gmail.com>
2017-10-10 21:59:37 +0300

    Fixed bugs

--- ar/ar.js
+++ ar/ar.js
@@ -45,7 +45,7 @@ AR.prototype.load = function () {
 	      if (this.video.paused || this.video.ended) return;
 	      this.ctx.fillRect(0, 0, w, h);
 				this.contextAnalysis(v);
-	   }.bind(this), 1000/30);
+	   }.bind(this), 1000/60);
 	}.bind(this), false);
 };
 
@@ -76,6 +76,11 @@ AR.prototype.contextAnalysis = function (v) {
 	ctx2.putImageData(image, 0, 0);
 
 	// Otsy
+	let histogram = [];
+	for (let i = 0; i<256; i++) {
+		histogram[i] = 0;
+	}
+
 	pixels = image.data;
 	for (let i = 0; i < pixels.length; i += 4) {
 		let red = pixels[i];
@@ -84,13 +89,48 @@ AR.prototype.contextAnalysis = function (v) {
 		pixels[i] = 0.2125*red + 0.7154*green + 0.0721*blue;
 		pixels[i+1] = 0.2125*red + 0.7154*green + 0.0721*blue;
 		pixels[i+2] = 0.2125*red + 0.7154*green + 0.0721*blue;
+		histogram[pixels[i]] += 1;
 	}
 
-	summary = (summary/pixels.length)*4*2;
+	let pixelsLen = pixels.length/4;
+	console.log(histogram);
+
+	summary = (summary/pixels.length)*4;
+
+	function otsu(histogram, pixelsNumber) {
+		var sum = 0
+		  , sumB = 0
+		  , wB = 0
+		  , wF = 0
+	      , mB
+		  , mF
+		  , max = 0
+		  , between
+		  , threshold = 0;
+		for (var i = 0; i < 256; ++i) {
+	      wB += histogram[i];
+	      if (wB == 0)
+	        continue;
+	      wF = pixelsNumber - wB;
+	      if (wF == 0)
+	        break;
+	      sumB += i * histogram[i];
+	      mB = sumB / wB;
+	      mF = (sum - sumB) / wF;
+	      between = wB * wF * Math.pow(mB - mF, 2);
+	      if (between > max) {
+	        max = between;
+	        threshold = i;
+	      }
+	    }
+	    return threshold;
+	}
+
+	let porog = otsu(histogram, pixelsLen);
 
 	for (let i = 0; i < pixels.length; i += 4) {
-		let current = (0.2125*pixels[i] + 0.7154*pixels[i+1] + 0.0721*pixels[i+2]);
-		if (current < summary) {
+		let current = (pixels[i] + pixels[i+1] + pixels[i+2])/3;
+		if (current > porog) {
 			pixels[i] = 255;
 			pixels[i+1] = 255;
 			pixels[i+2] = 255;
@@ -108,19 +148,19 @@ AR.prototype.contextAnalysis = function (v) {
 	canvas3.setAttribute('height', this.settings.height);
 	ctx3.putImageData(image, 0, 0);
 
-	// Sobel
-
-	let imageData = image;
-
-	var sobelData = Sobel(imageData);
-
-  // [sobelData].toImageData() returns a new ImageData object
-  var sobelImageData = sobelData.toImageData();
-
-	// To canvas #4
-	let canvas4 = document.querySelector("#canvas4");
-	let ctx4 = canvas4.getContext("2d");
-	canvas4.setAttribute('width', this.settings.width);
-	canvas4.setAttribute('height', this.settings.height);
-	ctx4.putImageData(sobelImageData, 0, 0);
+	// // Sobel
+	//
+	// let imageData = image;
+	//
+	// var sobelData = Sobel(imageData);
+	//
+  // // [sobelData].toImageData() returns a new ImageData object
+  // var sobelImageData = sobelData.toImageData();
+	//
+	// // To canvas #4
+	// let canvas4 = document.querySelector("#canvas4");
+	// let ctx4 = canvas4.getContext("2d");
+	// canvas4.setAttribute('width', this.settings.width);
+	// canvas4.setAttribute('height', this.settings.height);
+	// ctx4.putImageData(sobelImageData, 0, 0);
 };
