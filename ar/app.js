7cb5b9f21c98e0b0f3ae4d9de75cc2e873d7f215
Max Ersh <emvcoder@gmail.com>
2017-12-03 22:51:45 +0300

    Update app.js

--- ar/app.js
+++ ar/app.js
@@ -52,7 +52,6 @@ function snapshot(){
 }
 
 function drawCorners(markers){
-    "use strict";
     var corners, corner, i, j;
 
     context.lineWidth = 3;
@@ -79,7 +78,6 @@ function drawCorners(markers){
 }
 
 function drawId(markers){
-    "use strict";
     var corners, corner, x, y, i, j;
 
     context.strokeStyle = "blue";
@@ -117,154 +115,157 @@ function drawCenter(markers) {
             x3 = corners[3].x,
             y3 = corners[3].y;
 
-        var arrx = [x1, x2, x3];
-        var arry = [y1, y2, y3];
-        var xnum = 0,
-            ynum = 0;
+        // let arrx = [x1, x2, x3];
+        // let arry = [y1, y2, y3];
+        // let xnum = 0,
+        //     ynum = 0;
 
-        arry.forEach(function(ys) {
-            if (ys > y0) ynum++;
-        })
+        // arry.forEach(ys => {
+        //     if (ys > y0) ynum++;
+        // })
 
-        arrx.forEach(function(xs) {
-            if (x0 > xs) xnum++;
-        })
+        // arrx.forEach(function(xs) {
+        //     if (x0 > xs) xnum++;
+        // })
 
         var x = (x0+x1+x2+x3)/4;
         var y = (y0+y1+y2+y3)/4;
 
-        var AD = Math.sqrt(Math.pow(Math.abs(x0 - x3), 2)+Math.pow(Math.abs(y0 - y3), 2));
-        var AB = Math.sqrt(Math.pow(Math.abs(x0 - x1), 2)+Math.pow(Math.abs(y0 - y1), 2));
-        var BC = Math.sqrt(Math.pow(Math.abs(x1 - x2), 2)+Math.pow(Math.abs(y1 - y2), 2));
-        var CD = Math.sqrt(Math.pow(Math.abs(x2 - x3), 2)+Math.pow(Math.abs(y2 - y3), 2));
+        context.fillStyle = "blue";
+        context.fillRect(x-2, y-2, 4, 4);
 
-        var a = (AB+BC+CD+AD)/3;
+        // let AD = Math.sqrt(Math.pow(Math.abs(x0 - x3), 2)+Math.pow(Math.abs(y0 - y3), 2));
+        // let AB = Math.sqrt(Math.pow(Math.abs(x0 - x1), 2)+Math.pow(Math.abs(y0 - y1), 2));
+        // let BC = Math.sqrt(Math.pow(Math.abs(x1 - x2), 2)+Math.pow(Math.abs(y1 - y2), 2));
+        // let CD = Math.sqrt(Math.pow(Math.abs(x2 - x3), 2)+Math.pow(Math.abs(y2 - y3), 2));
 
-        // let k = ((AB - CD) > 0.1 || (AB - CD)) < -0.1 ? -1 : 1;
+        // let a = (AB+BC+CD+AD)*2/3;
 
-        // let alpha = (Math.PI/2-Math.acos(Math.abs(y-y3)/a))*Math.PI*2;
+        // // let k = ((AB - CD) > 0.1 || (AB - CD)) < -0.1 ? -1 : 1;
 
-        // context.fillStyle = "blue";
-        // context.fillRect(x3+CD/2, Math.abs(y2-y3)+Math.min(y2, y3), 4, 4);        
+        // // let alpha = (Math.PI/2-Math.acos(Math.abs(y-y3)/a))*Math.PI*2;
 
-        // let OR = Math.sqrt(Math.pow(CD/2, 2) + Math.pow(Math.abs(y2-y3)+Math.min(y2, y3) - y, 2));
+        // // context.fillStyle = "blue";
+        // // context.fillRect(x3+CD/2, Math.abs(y2-y3)+Math.min(y2, y3), 4, 4);        
 
-        var k = (AB > CD) ? 1 : -1;
+        // // let OR = Math.sqrt(Math.pow(CD/2, 2) + Math.pow(Math.abs(y2-y3)+Math.min(y2, y3) - y, 2));
 
-        // let alpha = k*(Math.PI/2 - Math.asin(Math.min(AB, CD)/Math.max(AB, CD)));
+        // let k = (AB > CD) ? 1 : -1;
 
-        var t = AD > BC ? 1 : -1;
+        // // let alpha = k*(Math.PI/2 - Math.asin(Math.min(AB, CD)/Math.max(AB, CD)));
 
-        if (ynum > 1) {
-            k = -k;
-            t = -t;
-        }
+        // let t = AD > BC ? 1 : -1;
 
-        // let betha = t*(Math.PI/2 - Math.asin(Math.min(AD, BC)/Math.max(AD, BC)));
-
-        var alpha = 3*k*Math.atan((Math.min(AB, CD)-Math.max(AB, CD))/Math.abs(y0-y3));
-        var betha = 3*t*Math.atan((Math.min(BC, AD)-Math.max(BC, AD))/Math.abs(y1-y2));
-        // console.log(alpha)
-
-        // if (xnum > 2) {
-        //     let m = betha;
-        //     betha = alpha;
-        //     alpha = betha;
+        // if (ynum > 1) {
+        //     k = -k;
+        //     t = -t;
         // }
 
-        // let gamma = Math.atan2(Math.abs(y0-y3), Math.abs(x0-x3));s
+        // // let betha = t*(Math.PI/2 - Math.asin(Math.min(AD, BC)/Math.max(AD, BC)));
 
-        xrotate = alpha;
-        yrotate = betha;
-        zrotate = zrotate+0.05;
+        // let alpha = 3*k*Math.atan((Math.min(AB, CD)-Math.max(AB, CD))/Math.abs(y0-y3));
+        // let betha = 3*t*Math.atan((Math.min(BC, AD)-Math.max(BC, AD))/Math.abs(y1-y2));
+        // // console.log(alpha)
 
-        context.fillStyle = "red";
-        context.fillRect(x - 2, y - 2, 4, 4);
+        // // if (xnum > 2) {
+        // //     let m = betha;
+        // //     betha = alpha;
+        // //     alpha = betha;
+        // // }
 
-        prevX = (3*x - window.innerWidth/2);
-        prevY = (window.innerHeight/2 - 3*y);
+        // // let gamma = Math.atan2(Math.abs(y0-y3), Math.abs(x0-x3));s
 
-        removeEntity(markers[i].id);
+        // xrotate = alpha;
+        // yrotate = betha;
+        // zrotate = zrotate+0.05;
 
-        createObjectMesh(markers[i].id, a);
-        scene.add(mesh);
+        // context.fillStyle = "red";
+        // context.fillRect(x - 2, y - 2, 4, 4);
 
-        mesh.position.x = prevX;
-        mesh.position.y = prevY;
-        mesh.position.z = a/Math.sqrt(2);
+        // prevX = (3*x - window.innerWidth/2);
+        // prevY = (window.innerHeight/2 - 3*y);
 
-        mesh.rotation.z = zrotate;
-        mesh.rotation.x = xrotate;
-        mesh.rotation.y = yrotate;
+        // removeEntity(markers[i].id);
 
-        clearTimeout(timeout[markers[i].id]);
-        timeout[markers[i].id] = setTimeout(removeEntity, 800, markers[i].id);
+        // createObjectMesh(markers[i].id, a);
+        // scene.add(mesh);
+
+        // mesh.position.x = prevX;
+        // mesh.position.y = prevY;
+        // mesh.position.z = a/Math.sqrt(2);
+
+        // mesh.rotation.z = zrotate;
+        // mesh.rotation.x = xrotate;
+        // mesh.rotation.y = yrotate;
+
+        // clearTimeout(timeout[markers[i].id]);
+        // timeout[markers[i].id] = setTimeout(removeEntity, 800, markers[i].id);
     }
-    renderer.render(scene, camera);
+    // renderer.render(scene, camera);
 }
 
-function createObjectMesh(id, side) {
-    var geometry;
+// function createObjectMesh(id, side) {
+//     var geometry;
 
-    switch (id) {
-        case 7:
-            geometry = new THREE.CubeGeometry(side, side, side);
-            break;
-        case 101:
-            geometry = new THREE.CubeGeometry(side, 2*side, side);
-            // geometry = new THREE.DodecahedronGeometry(side);
-            break;
-        case 102:
-            geometry = new THREE.ConeGeometry(side/2, side, 4);
-            break;
-        case 103:
-            geometry = new THREE.CylinderGeometry(side/2, side/2, side, 64);
-            break;
-        case 104:
-            geometry = new THREE.ConeGeometry(side/2, side, 64);
-            break;
-        case 105:
-            geometry = new THREE.CylinderGeometry(side, side, 2*side, 3);
-            break;
-        case 106:
-            geometry = new THREE.TetrahedronGeometry(side/2);
-            break;
-        case 107:
-            geometry = new THREE.OctahedronGeometry(side/2);
-            break;
-        case 108:
-            geometry = new THREE.DodecahedronGeometry(side/2);
-            break;
-        case 109:
-            geometry = new THREE.IcosahedronGeometry(side/2);
-            break;
-        default:
-            geometry = null;
-  }
+//     switch (id) {
+//         case 7:
+//             geometry = new THREE.CubeGeometry(side, side, side);
+//             break;
+//         case 101:
+//             geometry = new THREE.CubeGeometry(side, 2*side, side);
+//             // geometry = new THREE.DodecahedronGeometry(side);
+//             break;
+//         case 102:
+//             geometry = new THREE.ConeGeometry(side/2, side, 4);
+//             break;
+//         case 103:
+//             geometry = new THREE.CylinderGeometry(side/2, side/2, side, 64);
+//             break;
+//         case 104:
+//             geometry = new THREE.ConeGeometry(side/2, side, 64);
+//             break;
+//         case 105:
+//             geometry = new THREE.CylinderGeometry(side, side, 2*side, 3);
+//             break;
+//         case 106:
+//             geometry = new THREE.TetrahedronGeometry(side/2);
+//             break;
+//         case 107:
+//             geometry = new THREE.OctahedronGeometry(side/2);
+//             break;
+//         case 108:
+//             geometry = new THREE.DodecahedronGeometry(side/2);
+//             break;
+//         case 109:
+//             geometry = new THREE.IcosahedronGeometry(side/2);
+//             break;
+//         default:
+//             geometry = null;
+//   }
 
-  var material = new THREE.MeshNormalMaterial();
-  mesh = new THREE.Mesh(geometry, material);
-  mesh.name = id;
-}
+//   var material = new THREE.MeshNormalMaterial();
+//   mesh = new THREE.Mesh(geometry, material);
+//   mesh.name = id;
+// }
 
-function removeEntity(id) {
-    var selectedObject = scene.getObjectByName(id);
-    scene.remove(selectedObject);
-}
+// function removeEntity(id) {
+//     var selectedObject = scene.getObjectByName(id);
+//     scene.remove(selectedObject);
+// }
 
 function init() {
-    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.z = 1000;
+    // camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
+    // camera.position.z = 1000;
 
-    scene = new THREE.Scene();
-    renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    // scene = new THREE.Scene();
+    // renderer = new THREE.WebGLRenderer({ alpha: true });
+    // renderer.setPixelRatio( window.devicePixelRatio );
+    // renderer.setSize( window.innerWidth, window.innerHeight );
+    // document.body.appendChild( renderer.domElement );
 }
 
-function animate() {
-    mesh.rotation.y += 0.005;
-}
+// function animate() {
+//     mesh.rotation.y += 0.005;
+// }
 
 window.onload = onLoad;
