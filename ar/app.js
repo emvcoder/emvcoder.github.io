57048e791bb33d958a3c7abb6521b57bb6cd6177
Max Ersh <emvcoder@gmail.com>
2017-12-07 21:13:48 +0300

    Update app.js

--- ar/app.js
+++ ar/app.js
@@ -12,7 +12,6 @@ function onLoad(){
     canvas.width = window.innerWidth/scale;
     canvas.height = window.innerHeight/scale;
 
-    navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia;
     function successCallback(stream) {
         if (window.webkitURL) {
             video.src = window.webkitURL.createObjectURL(stream);
